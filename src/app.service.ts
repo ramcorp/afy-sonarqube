7bd6ac5b9a0faecafb5a9f8e88cedb6c38c63241
Breno Alves <breno550@hotmail.com>
2023-01-16 08:09:08 -0300

    chore: sonarqube test

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,20 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  test(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Hello World!');
+      }, 5000);
+    });
+  }
+
+  testCodeSmells(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('Hello World!');
+      }, 5000);
+    });
+  }
 }
